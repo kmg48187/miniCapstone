0b2866d2c498f0d5ef020733bcf902c6782046f8
Ethan Toups <ethantoups05@gmail.com>
2025-07-21 21:11:58 -0500

    Fixed avatar encoding

--- db/seed.js
+++ db/seed.js
@@ -9,15 +9,13 @@ await db.end();
 console.log("Database seeded.");
 
 async function seed() {
-  const imgData = fs.readFileSync("businessman.jpg", { encoding: "base64" });
-
   for (let i = 1; i < 10; i++) {
     const department = {
       name: "Department" + i,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis commodo elit, in finibus libero. Proin venenatis quam tortor, id euismod ligula dapibus nec.",
-      banner_image: imgData,
-      contact_info: "fake-email@fake-email.com",
+      banner_image: "https://i.pravatar.cc/300",
+      contact_info: "fake-email@gmail.com",
     };
     await createDepartment(
       department.name,
@@ -30,9 +28,9 @@ async function seed() {
   for (let i = 1; i < 10; i++) {
     const faculty = {
       name: "Employee" + i,
-      email: "fake-email@fake-email.com",
+      email: "fake-email@gmail.com",
       bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis commodo elit, in finibus libero. Proin venenatis quam tortor, id euismod ligula dapibus nec.",
-      profile_pic: imgData,
+      profile_pic: "https://i.pravatar.cc/300",
       department_id: Math.floor(Math.random() * 9) + 1,
     };
     await createFaculty(
